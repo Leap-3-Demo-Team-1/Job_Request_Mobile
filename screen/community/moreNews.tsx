eb82842b5e32d0c6383d0a84c337fded7e676e2b
leap3-Tengis <leap3-tengis@iMac-20s-iMac.local>
2023-05-08 11:54:50 +0800

    first

---
+++ screen/community/moreNews.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native"
+
+const MoreNews = () => {
+    return(
+        <View>
+            <Text>fjhaiufsdfaesfseffiuabwuifa</Text>
+        </View>
+    )
+}
+export default MoreNews;
